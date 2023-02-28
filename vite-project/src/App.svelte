015d5c537aa1b2fa072bca3a6c8028ad89731126
karim-daman <38837234+karim-daman@users.noreply.github.com>
2023-02-28 06:48:07 +0100

    update

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -94,8 +94,7 @@
 
   @font-face {
     font-family: "My Custom Font";
-    /* src: url("/Font/Next-Mono-Thin.otf") format("opentype"); */
-    src: url("./Font/NEXT-Mono-Thin.otf") format("opentype");
+    src: url("/Font/NEXT-Mono-Thin.otf") format("opentype");
   }
 
   main {
