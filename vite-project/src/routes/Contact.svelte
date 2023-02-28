adb01dd6a4d164efdfca00104716b60b0807df02
karim-daman <38837234+karim-daman@users.noreply.github.com>
2023-02-28 07:23:16 +0100

    update

--- vite-project/src/routes/Contact.svelte
+++ vite-project/src/routes/Contact.svelte
@@ -3,6 +3,17 @@
   import { fade } from "svelte/transition";
 
   let name, company, profession;
+
+  let tel = "+31 (0) 6 44 01 73 66";
+  let email = "kerberos.and.styx@gmail.com";
+
+  function copyTel() {
+    navigator.clipboard.writeText(tel);
+  }
+
+  function copyEmail() {
+    navigator.clipboard.writeText(email);
+  }
 </script>
 
 <div class="fixed inset-0 bg-black opacity-60" />
@@ -20,9 +31,30 @@
           <br /><br />
           <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs float-left ">
             EMAIL <a class="text-indigo-500 leading-relaxed  blur-sm hover:blur-none ">kerberos.and.styx@gmail.com</a>
+
+            <button class=" rounded-sm active:scale-90" on:click={copyEmail}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+              </svg>
+            </button>
           </h2>
           <br />
-          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs float-left ">Tel <a class="leading-relaxed blur-sm hover:blur-none ">+31 (0) 6 44 01 73 66</a></h2>
+          <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs float-left ">
+            Tel
+            <a id="tel" class="leading-relaxed blur-sm hover:blur-none ">+31 (0) 6 44 01 73 66</a>
+
+            <button class=" rounded-sm active:scale-90 " on:click={copyTel}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+              </svg>
+            </button>
+          </h2>
           <br /><br />
 
           <span class="inline-flex sm:ml-auto sm:mt-0  justify-center sm:justify-start ">
